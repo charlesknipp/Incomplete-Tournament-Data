7a1be6b67d4d7afd16c406dec7c8b21f1101517e
classicharlie <cknipp@asu.edu>
2019-11-16 12:34:09 -0700

    Remove comment

--- clean.py
+++ clean.py
@@ -141,10 +141,7 @@ def merge(szn,simple):
     return df
 
 
-# I am running into an issue adding the hashes to the datasets due to a nice
-# little OverflowError from ... + 1000 * d['L'] + ...
-
-for szn in range(2000, 2014):
+for szn in range(1978, 2014):
 
     df = merge(szn, 'n')
     print(szn, df.dtypes)
